8267e2bd1ccc5f45faf4739b5a6e82b9e848a0d3
czM1K3 <misamadera1@gmail.com>
2020-11-18 22:11:03 +0100

    Tuned X and Y steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -745,7 +745,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 142.86 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 101.27, 100.76, 400, 142.86 }
 
 /**
  * Default Max Feed Rate (mm/s)
